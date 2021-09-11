fd721b921be48587d33f5eed5fb9fcdd012f2cb8
dclark <dclark@student.42.fr>
2021-09-11 12:23:54 +0200

    Premier test mutex

--- struct_create.c
+++ struct_create.c
@@ -6,7 +6,7 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 12:17:07 by dclark            #+#    #+#             */
-/*   Updated: 2021/09/10 14:33:56 by dclark           ###   ########.fr       */
+/*   Updated: 2021/09/11 12:21:20 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ typedef struct	s_1 {
 //	long	time_value;
 	struct timeval toto;
 	int		ID;
+	int		*value;
 }				f_1;
 
 void	*worm(void *arg)
@@ -27,14 +28,14 @@ void	*worm(void *arg)
 	struct timeval timevals;
 	f_1 *st = arg;
 	int flag = 1;
-	printf("ID = %d\n", st->ID);
+	printf("ID: %.2d\n", st->ID);
 	while (flag)
 	{
 		usleep(400);
 		gettimeofday(&timevals, 0);
 		if (st->toto.tv_sec == timevals.tv_sec)
 		{
-			printf("ID: %d time = %ld\n", st->ID, timevals.tv_sec);
+			printf("ID: %.2d time = %.2ld\n", st->ID, timevals.tv_sec);
 			flag = 0;
 		}
 	}
@@ -43,8 +44,14 @@ void	*worm(void *arg)
 
 int main(int ac, char **av)
 {
+	if (ac != 3)
+	{
+		printf("manque un argument\n");
+		return 0;
+	}
 	struct timeval mani;
 	f_1 *input;
+	int	val_input = atoi(av[2]);
 	int num_of_philo = atoi(av[1]);
 	int index = 0;
 	long timet;
@@ -59,9 +66,10 @@ int main(int ac, char **av)
 	{
 		input[index].ID = index;
 		input[index].toto.tv_sec = timet;
+		input[index].value = &val_input;
 		pthread_create(&ptab[index], NULL, *worm, &input[index]);
 		index++;
-		usleep(100);
+		usleep(75);
 	}
 	for (int i = 0; i < num_of_philo; i++)
 		pthread_join(ptab[i], NULL);
